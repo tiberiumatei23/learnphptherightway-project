e66c8530edce07e6f2c1418919445f753a472075
Tiberiu Matei <matei_tiberiu99@yahoo.com>
2022-07-11 17:05:04 +0300

    Bug fixing

--- public/index.php
+++ public/index.php
@@ -10,7 +10,8 @@ define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
 
-require APP_PATH . "App.php";
+require APP_PATH."App.php";
+
 $files = getTransactionFiles(FILES_PATH);
 $transactions=[];
 foreach ($files as $file){
